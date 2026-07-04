e1af1541c23c79e0e6a836c0a55bc266e93e1550
Daniel Eduardo <alejosdaniel52@gmail.com>
2026-07-04 16:53:35 -0400

    Arreglar vista de perfil: cambiar x-app-layout por layouts.app

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,30 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+@extends('layouts.app')
+@section('titulo', 'Mi Perfil')
+@section('contenido')
+<div class="row g-4">
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-header">Información del Perfil</div>
+            <div class="card-body">
+                @include('profile.partials.update-profile-information-form')
             </div>
         </div>
     </div>
-</x-app-layout>
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-header">Cambiar Contraseña</div>
+            <div class="card-body">
+                @include('profile.partials.update-password-form')
+            </div>
+        </div>
+    </div>
+    <div class="col-12">
+        <div class="card border-danger">
+            <div class="card-header text-danger">Eliminar Cuenta</div>
+            <div class="card-body">
+                @include('profile.partials.delete-user-form')
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
